cc08d679f6e71e4b7ad524ef54faef8087ed5cdd
abdulramiz37 <abdulramiz007@gmail.com>
2022-03-19 23:34:03 +0530

    first comm

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,14 +1,7 @@
 import * as React from 'react';
-import Drawer from '@mui/material/Drawer';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
+import { Drawer, Toolbar, List, Divider, ListItem, Box } from '@mui/material';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import Box from '@mui/material/Box';
 import PersonIcon from '@mui/icons-material/Person';
 import AlbumIcon from '@mui/icons-material/Album';
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
